a85fd05c4021d67e49f02c96f7a4207548e85347
William-tecnico <wil.dossantos@gmail.com>
2021-10-06 16:24:13 -0300

    teste alterações

---
+++ js/main.js
@@ -0,0 +1,68 @@
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+/*
+var count = 1;
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt ("qual a sua idade");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+/*
+var frutas = [{nome:"maça", cor:"Vermelha"}, {nome:"Uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome:"maça", cor:"Vermelha"};
+console.log(fruta.cor);
+alert(fruta.nome);
+*/
+
+
+
+//var fruta = ["maça", "pêra", "Laranja"];
+//fruta.push("uva");
+//fruta.pop();
+//console.log (fruta);
+//alert(fruta);
+//console.log(fruta.reverse());
+//console.log(fruta.toString());
+//console.log(fruta.join(" - "));
+
+
+
+/*
+var nome = "William dos santos";
+var idade = 37;
+var idade2 = 10;
+var frase = "japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("japão", "Brasil"));
+//console.log(frase.replace("time", "programador"));
+//console.log(frase);
+alert(frase.replace("japão", "brasil"));
+console.log(frase.toUpperCase());
+*/
